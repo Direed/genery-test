f435dae7b41b1c1fc221b1515e1515bcb1b1da5e
bohdan-kiev-ua <bohdan.kiev.ua@gmail.com>
2023-11-26 15:14:45 +0200

    scroll restoration fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import '@/styles/main.scss'
+import { useEffect } from "react";
 
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    history.scrollRestoration = "manual"
+  }, [])
   return <Component {...pageProps} />
 }
